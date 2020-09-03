9cf2527d4a7fe2cf8f12f60dcca523630b1ef5d5
Alexandre Pereira <alempereira@hotmail.com>
2020-09-03 19:34:59 -0300

    JWT Auth - User model

--- node-express-jwt-auth/app.js
+++ node-express-jwt-auth/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-//const authRoutes = require("./routes/authRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -9,7 +9,7 @@ const PORT = 3000;
 
 // middleware
 app.use(express.static('public'));
-app.use(express.json);
+app.use(express.json());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -21,10 +21,10 @@ const dbCluster = "tutoriais";
 const dbName = "jwt-auth";
 const dbURI = `mongodb+srv://${dbUser}:${dbPassword}@${dbCluster}.7kiyu.gcp.mongodb.net/${dbName}`;
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(PORT, () => console.log(`Listening on port ${PORT}...`)))
+  .then((result) => app.listen(PORT, (e) => console.log(`Listening on port ${PORT}...`)))
   .catch((err) => console.log(err));
 
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
-//app.use(authRoutes);
+app.use(authRoutes);
--- node-express-jwt-auth/controllers/authController.js
+++ node-express-jwt-auth/controllers/authController.js
@@ -1,17 +1,29 @@
+const User = require("../models/User");
+
+const handleErrors = (error) => {
+    console.log(error.message, error.code);
+}
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
 
-module.exports.signup_post = (req, res) => {
+module.exports.signup_post = async (req, res) => {
     const {email, password} = req.body;
-    res.send(`email: ${email}, password: ${password}`);
+    try {
+        const user = await User.create({email, password});        
+        res.status(201).json(user);
+    } catch (error) {
+        const errors = handleErrors(error);
+        res.status(400).send("Error, user not created");
+    }
 }
 
 module.exports.login_get = (req, res) => {
     res.render('login');
 }
 
-module.exports.login_post = (req, res) => {
+module.exports.login_post = async (req, res) => {
     const {email, password} = req.body;
     res.send(`email: ${email}, password: ${password}`);
 }
