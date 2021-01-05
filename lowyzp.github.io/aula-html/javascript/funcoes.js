6c63218e68afcdc7696f5897cbb88107c72c2429
Alexandre Pereira <alempereira@hotmail.com>
2021-01-05 14:00:24 -0300

    Remover aulas-html do Luiz

--- lowyzp.github.io/aula-html/javascript/funcoes.js
+++
@@ -1,5 +0,0 @@
-function calc_total(){
-    var qtd = parseInt(document.getElementById("cqtd").value);
-    tot = qtd * 1500
-    document.getElementById("cpre").value = tot
-}
--- lowyzp.github.io/aula-js/main.js
+++
@@ -1,9 +0,0 @@
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://lowyzp.github.io');
-xhr.send(null);
-
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4){
-        console.log(JSON.parse(xhr.responseText));
-    }
-}
